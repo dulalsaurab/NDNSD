debc8afe0fc303cfdf3f944f8dc0a447a06b0f68
Saurab Dulal <sdulal@memphis.edu>
2020-07-22 12:46:45 -0500

    exp4 3p6c

--- experiments/ndnsd-experiments.py
+++ experiments/ndnsd-experiments.py
@@ -11,15 +11,24 @@ from minindn.helpers.nfdc import Nfdc
 from subprocess import Popen, PIPE
 import os
 from minindn.apps.nlsr import Nlsr
+from minindn.helpers.ndn_routing_helper import NdnRoutingHelper
+import subprocess
 
 import time
 from random import randrange
 
-numberOfUpdates = 100
+numberOfUpdates = 300
 jitter = 50
 
 def cleanUp():
     pass
+
+def registerRouteToAllNeighbors(ndn, host, syncPrefix):
+    for node in ndn.net.hosts:
+        for neighbor in node.connectionsTo(host):
+            ip = node.IP(neighbor[0])
+            Nfdc.createFace(host, ip)
+            Nfdc.registerRoute(host, syncPrefix, ip)
 
 class NDNSDExperiment():
 
@@ -36,7 +45,10 @@ class NDNSDExperiment():
     self.ndn.start()
     time.sleep(2)
     nfds = AppManager(self.ndn, self.ndn.net.hosts, Nfd, logLevel='DEBUG')
-    nlsrs = AppManager(self.ndn, self.ndn.net.hosts, Nlsr)
+    # nlsrs = AppManager(self.ndn, self.ndn.net.hosts, Nlsr)
+
+    # info('Starting NFD on nodes\n')
+    # nfds = AppManager(ndn, ndn.net.hosts, Nfd)
 
     for host in self.ndn.net.hosts:
       host.cmd('tshark -o ip.defragment:TRUE -o ip.check_checksum:FALSE -ni any -f "udp port 6363" -w {}.pcap &> /dev/null &'
@@ -45,7 +57,7 @@ class NDNSDExperiment():
       time.sleep(0.005)
 
 
-    time.sleep(60)
+    # time.sleep(6)
      # copy the test.info file to system first and from there copy it to node directory
     Popen(['cp', 'test.info', '/usr/local/etc/ndn/ndnsd_default.info'],
            stdout=PIPE, stderr=PIPE).communicate()
@@ -56,7 +68,7 @@ class NDNSDExperiment():
     for host in self.producer_nodes: # if host.name not in consumer:
       hostName = host.name
       hostInfoFile = self.args.workDir+'/'+hostName+'/ndnsd_'+hostName+'.info'
-      appPrefix = '/'+hostName+'/service-info'
+      appPrefix = '/ndnsd/'+hostName+'/service-info'
       
       # hostInfo[host.name] = [hostInfoFile, appPrefix, '/printer']
       # appending filename just in case if we need in future
@@ -67,17 +79,19 @@ class NDNSDExperiment():
 
       host.cmd('infoedit -f {} -s required.appPrefix -v {}'.format(hostInfoFile, appPrefix))
       
-      cmd = 'nlsrc advertise {}'.format(appPrefix)
-      host.cmd(cmd)
+      # cmd = 'nlsrc advertise {}'.format(appPrefix)
+      # host.cmd(cmd)
       
-      cmd = 'nlsrc advertise {}{}'.format('/discovery/', self.producers[hostName][0])
-      host.cmd(cmd)
+      # cmd = 'nlsrc advertise {}{}'.format('/discovery/', self.producers[hostName][0])
+      # host.cmd(cmd)
+
       Nfdc.setStrategy(host, self.producers[hostName][0], Nfdc.STRATEGY_MULTICAST)
 
-      cmd = 'export NDN_LOG=ndnsd.*=DEBUG:psync.*=TRACE:sync.*=TRACE'
+      # uncomment to enable sync log
+      cmd = 'export NDN_LOG=ndnsd.*=TRACE' # :psync.*=TRACE:sync.*=TRACE'
       host.cmd(cmd)
 
-      cmd = 'ndnsd-producer {} 0 &> {}/{}/producer.log &'.format(hostInfoFile, self.args.workDir, hostName)
+      cmd = 'ndnsd-producer {} 1 &> {}/{}/producer.log &'.format(hostInfoFile, self.args.workDir, hostName)
       host.cmd(cmd)
       time.sleep(10)
 
@@ -86,13 +100,14 @@ class NDNSDExperiment():
     for consumer in self.consumer_nodes:
       cName = consumer.name
 
-      cmd = 'export NDN_LOG=ndnsd.*=TRACE:psync.*=TRACE:sync.*=TRACE'
+      # uncomment to enable sync log
+      cmd = 'export NDN_LOG=ndnsd.*=TRACE' #:psync.*=TRACE:sync.*=TRACE'
       consumer.cmd(cmd)
 
       # set multi-cast strategy for sync prefix
       Nfdc.setStrategy(consumer, self.consumers[cName], Nfdc.STRATEGY_MULTICAST)
 
-      cmd = 'ndnsd-consumer -s {} &> {}/{}/consumer.log -c 1 -p 0 &'.format(self.consumers[cName],
+      cmd = 'ndnsd-consumer -s {} &> {}/{}/consumer.log -c 1 -p 1 &'.format(self.consumers[cName],
                                                                        self.args.workDir, cName)
       consumer.cmd(cmd)
       time.sleep(2)
@@ -100,15 +115,31 @@ class NDNSDExperiment():
 
 if __name__ == '__main__':
     setLogLevel('info')
+
+    subprocess.call(['rm','-r','/tmp/minindn/'])
+
     producers = dict()
     consumers = dict()
-    producers['p1'] = ['printer', 800] #{"<sp-name>": ['service-name', 'lifetime in ms']}
-    producers['p2'] = ['printer', 800]
-    # consumers = {'a':'printer', 'b':'printer', 'c':'printer','d':'printer', 'e':'printer'} #['<consumer-name>']
-    consumers = {'a':'printer', 'b':'printer'} #['<consumer-name>']
+    producers['p1'] = ['printer', 900] #{"<sp-name>": ['service-name', 'lifetime in ms']}
+    producers['p2'] = ['printer', 900]
+    producers['p3'] = ['printer', 900]
+    consumers = {'a':'printer', 'b':'printer', 'c':'printer','d':'printer', 'e':'printer', 'f':'printer'} #['<consumer-name>']
+    # consumers = {'a':'printer', 'b':'printer'} #['<consumer-name>']
 
     ndn = Minindn()
     exp = NDNSDExperiment(ndn.args, producers, consumers)
+
+    # For all host, pass ndn.net.hosts or a list, [ndn.net['a'], ..] or [ndn.net.hosts[0],.]
+    info('Adding static routes to NFD\n')
+    grh = NdnRoutingHelper(ndn.net)
+
+    for host in exp.producer_nodes:
+      hostName = host.name
+      appPrefix = '/ndnsd'+hostName+'/service-info'
+      discoveryPrefix = '/discovery/{}'.format(exp.producers[hostName][0])
+      grh.addOrigin([host], [appPrefix, discoveryPrefix])
+
+    grh.calculateNPossibleRoutes(1)
 
     exp.startProducer()
     exp.startConsumer()
@@ -121,14 +152,15 @@ if __name__ == '__main__':
     for host in exp.producer_nodes:
       cmd = 'ndnsd-reload -c {} -i {} -r {} &> {}/{}/reload.log &'.format(numberOfUpdates,
                                                                     exp.producers[host.name][1]-10,
-                                                                    300, ndn.args.workDir,
+                                                                    200, ndn.args.workDir,
                                                                     host.name)
       host.cmd(cmd)
 
     # approximate time to complete the experiment
     print("sleep approximately {} seconds to complete the experiment".format(2*(numberOfUpdates + jitter)))
-    time.sleep(3*(numberOfUpdates + jitter))
+    time.sleep(2*(numberOfUpdates + jitter))
     print("Experiment completed")
 
     ndn.stop()
+    # MiniNDNCLI(ndn.net)
 
--- experiments/stat-process.py
+++ experiments/stat-process.py
@@ -21,8 +21,6 @@ def dump(data, filename):
   h_l = [item[0] for item in data] 
   data = np.transpose([i[1:] for i in data])
   header = ','.join(h_l)
-  print(header)
-  exit()
   np.savetxt(filename, data, delimiter=",", header=header, fmt='%1.5f')
 
 def drawGraph(r_final):
@@ -41,31 +39,34 @@ def getDiff(send, received):
 
 if __name__ == '__main__':
 
-  rootDir = "/Users/saurabdulal/Documents/IMAGES/mini-ndn/ndn-src/NDNSD/experiments/real-exp/phase1/single-hop/2p-8c_same_room/"
-  topo = {'c1p1':1, 'c1p2':1, 'c1p3':1,
-          'c2p1':1, 'c2p2':1, 'c2p3':1,
-          'c3p1':1, 'c3p2':1, 'c3p3':1,
-          'c4p1':1, 'c4p2':1, 'c4p3':1,
-          'c5p1':1, 'c5p2':1, 'c5p3':1,
-          'c6p1':1, 'c6p2':1, 'c6p3':1,
-          'macp1':1,'macp2':1, 'macp3':1,
+  rootDir = "/Users/saurabdulal/Documents/IMAGES/mini-ndn/ndn-src/NDNSD/experiments/phase3/multi-hop/emulation/3p6c/take2"
+  topo = {'c1p1':1, 'c1p2':2, 'c1p3':4,
+          'c2p1':1, 'c2p2':2, 'c2p3':4,
+          'c3p1':1, 'c3p2':2, 'c3p3':4,
+          'c4p1':2, 'c4p2':1, 'c4p3':3,
+          'c5p1':2, 'c5p2':1, 'c5p3':1,
+          'c6p1':3, 'c6p2':2, 'c6p3':2,
           }
 
-  cons = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'mac']
   prod = ['p1', 'p2', 'p3']
-  
-  # s_g = [] #send grep
-  # r_g =[] #receiver grep
-  # for i in prod:
-  #     s_g.append("Sending interest: /{}".format(i))
-  #     r_g.append("Data received for: /{}".format(i))
+  cons = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6']
+
   hop = defaultdict(list)
   for i in cons:
     file1 = "{}/{}/consumer.log".format(rootDir, i)
     file2 = "{}/{}/consumer.log".format(rootDir, i)
     
-    send = processLogFile(file1, ["Sending interest: /ndnsd/p1", "Sending interest: /ndnsd/p2", "Sending interest: /ndnsd/p3"])
-    received = processLogFile(file2, ["Data received for: /ndnsd/p1", "Data received for: /ndnsd/p2", "Data received for: /ndnsd/p3"])
+    send = processLogFile(file1, [
+                                  "Transmission count: 1 - Sending interest: /p1/",
+                                  "Transmission count: 1 - Sending interest: /p2/",
+                                  "Transmission count: 1 - Sending interest: /p3/",
+                                  ])
+    received = processLogFile(file2, [
+                                      "Data received for: /p1", 
+                                      "Data received for: /p2", 
+                                      "Data received for: /p3"
+                                      ])
+
     r_final = getDiff(send, received)
     for p in prod:
       c_p = [r_final[x] for x in r_final if p in x]
@@ -75,8 +76,5 @@ if __name__ == '__main__':
 
   for i in hop:
     dump(hop[i], str(i)+'.csv')
-
-
-
 
 
