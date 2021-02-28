8ee7986aa78e120761091ab1d64dddeab9c125ae
dulalsaurab <sdulal@memphis.edu>
2021-02-28 22:27:03 +0000

    ndnsd-ip exp

--- experiments/ndnsd-ip-ndn.py
+++ experiments/ndnsd-ip-ndn.py
@@ -12,16 +12,14 @@ from minindn.wifi.minindnwifi import MinindnWifi
 from minindn.util import MiniNDNWifiCLI, getPopen
 
 def setMTUsize(ndn, mtu):
-    for host in ndn.net.hosts:
+    for host in ndn.net.stations:
         for intf in host.intfList():
             host.cmd("ifconfig {} mtu {}".format(intf, mtu))
 
 def setTsharkLog (net):
-    for host in net.hosts:
-        print ("came here:", host.name)
-        # host.cmd('tshark -o ip.defragment:TRUE -o ip.check_checksum:FALSE -ni any -f "port 6363" -w /tmp/{}.tshark &'.format(host.name))
-        print(host.cmd('tshark -o ip.defragment:TRUE -o ip.check_checksum:FALSE -ni any -f "udp port 5353" -w {}.pcap &> /dev/null &'.format(host.name)))
-        # host.cmd('sudo tcpdump port 5353 2>&1 > abc.txt &')   
+    for host in net.stations:
+        host.cmd('tshark -o ip.defragment:TRUE -o ip.check_checksum:FALSE -ni any -f "udp port 5353" -w {}.pcap &> /dev/null &'.format(host.name))
+        host.cmd('ndndump -i any &> {}.ndndump &'.format(host.name))
     time.sleep(5)
         # host.cmd('ndndump -i any &> {}.ndndump &'.format(host.name))
 
@@ -36,25 +34,27 @@ def start(ndn):
     # time.sleep(50)
     # Popen(['cp', 'test.info', '/usr/local/etc/ndn/ndnsd_default.info'], stdout=PIPE, stderr=PIPE).communicate()
     # print ("did i came here")
-    # setTsharkLog(ndn.net)
+    setTsharkLog(ndn.net)
 
 def startProducer (producer):
     # use avahi and publish service
-    producer.cmd('tshark -o ip.defragment:TRUE -o ip.check_checksum:FALSE -ni any -f "udp port 5353" -w {}.pcap &> /dev/null &'.format(producer.name))
-    cmd = 'echo "Producer publish" | ts "%Y%m%d-%H:%M:%.S" &> avahi.log &'
+    # producer.cmd('tshark -o ip.defragment:TRUE -o ip.check_checksum:FALSE -ni any -f "udp port 5353" -w {}.pcap &> /dev/null &'.format(producer.name))
+    cmd = 'echo "Producer publish" | ts `date +%s.%N` &> avahi.log &'
     producer.cmd(cmd)
-    cmd = 'avahi-publish-service {} {} {} {} | ts  "%Y%m%d-%H:%M:%.S" &>> avahi.log &'.format('printer', '_ndnsd._udp', 5683, '/mylight-myhouse')
+    cmd = 'avahi-publish-service {} {} {} {} | ts `date +%s.%N` &>> avahi.log &'.format('printer', '_ndnsd._udp', 5683, '/mylight-myhouse')
     producer.cmd(cmd)
     time.sleep(0.1)
     # Popen(['cp', 'test.info', '/usr/local/etc/ndn/ndnsd_default.info'], stdout=PIPE, stderr=PIPE).communicate() 
 
 def startConsumer (consumers):
     for consumer in consumers:
-        consumer.cmd('tshark -o ip.defragment:TRUE -o ip.check_checksum:FALSE -ni any -f "udp port 5353" -w {}.pcap &> /dev/null &'.format(consumer.name))  
-        cmd = 'echo "fetching service" | ts "%Y%m%d-%H:%M:%.S" &> avahi.log &'
+        # consumer.cmd('tshark -o ip.defragment:TRUE -o ip.check_checksum:FALSE -ni any -f "udp port 5353" -w {}.pcap &> /dev/null &'.format(consumer.name))  
+        # for i in range(0, 10):i
+        i=0
+        cmd = 'echo "Fetching service, consumer: {}" | ts `date +%s.%N` &> avahi_{}.log &'.format(str(i), str(i))
         consumer.cmd (cmd) 
-        cmd = 'avahi-browse -rt {} | ts "%Y%m%d-%H:%M:%.S" &>> avahi.log &'.format('_ndnsd._udp')
-        consumer.cmd (cmd)
+        cmd = 'avahi-browse -rt {} | ts `date +%s.%N` &>> avahi_{}.log &'.format('_ndnsd._udp', str(i))
+        consumer.cmd(cmd)
 
 if __name__ == '__main__':
     subprocess.call(['rm','-rf','/tmp/minindn/*'])
@@ -63,12 +63,9 @@ if __name__ == '__main__':
     ndnwifi = MinindnWifi()
     producers = ndnwifi.net['p1']
     consumers = [ndnwifi.net['c1']]
-    # consumers = [ndn.net['b'], ndn.net['c'], ndn.net['d']] 
     start(ndnwifi)
-
     startProducer(producers)
     startConsumer(consumers)
-
     MiniNDNWifiCLI(ndnwifi.net)
     subprocess.call(['killall','tshark'])
     ndnwifi.stop()
